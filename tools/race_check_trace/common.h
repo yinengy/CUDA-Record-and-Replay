63a86c1c1696501f084bfc799e74dc798a7e9037
yinengy <yinengy@umich.edu>
2020-07-16 22:40:43 +0000

    update copyright information

--- tools/race_check_trace/common.h
+++ tools/race_check_trace/common.h
@@ -1,39 +1,11 @@
 /* 
- * An NVBit tool, which will detect conflict memory access in the kernel.
- * The raw output will be processed by a Pytyhon script
- *
- * The tool is based on thetool (mem_trace) in nvbit_release
- * the original code is modified and extended to support the use of detecting data races
+ * definition of mem_access_t
  *
  * Yineng Yan (yinengy@umich.edu), 2020
  */
 
-/* Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *  * Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *  * Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *  * Neither the name of NVIDIA CORPORATION nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
- * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+#ifndef COMMON_H
+#define COMMON_H
 
 #include <stdint.h>
 
@@ -49,3 +21,5 @@ typedef struct {
     int SFR_id;  // id of synchronization-free regions
     uint64_t addrs[32];
 } mem_access_t;
+
+#endif /* COMMON_H */
--- tools/race_check_trace/inject_funcs.cu
+++ tools/race_check_trace/inject_funcs.cu
@@ -8,34 +8,6 @@
  * Yineng Yan (yinengy@umich.edu), 2020
  */
 
-
-/* Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *  * Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *  * Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *  * Neither the name of NVIDIA CORPORATION nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
- * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 #include <stdint.h>
 #include <stdio.h>
 
@@ -60,28 +32,30 @@ extern "C" __device__ __noinline__ void instrument_mem(int pred,
         return;
     }
 
+    /* common information of this warp */
     int l_thread_id = (threadIdx.z * (blockDim.x * blockDim.y)) +
                       (threadIdx.y * blockDim.x) + threadIdx.x;
 
-    int active_mask = ballot(1);
-    const int laneid = l_thread_id & 31;  /* use get_laneid() is less efficient */
-    const int first_laneid = __ffs(active_mask) - 1;
-
     mem_access_t ma;
 
-    /* collect memory address information from other threads */
-    for (int i = 0; i < 32; i++) {
-        ma.addrs[i] = shfl(addr, i);
-    }
-
-    ma.block_id = blockIdx.x + blockIdx.y * gridDim.x +
+    int block_id = blockIdx.x + blockIdx.y * gridDim.x +
                    gridDim.x * gridDim.y * blockIdx.z;
+    ma.block_id = block_id
     ma.warp_id = get_warpid();
     ma.func_id = func_id;
     ma.inst_id = inst_id;
     ma.is_shared_memory = is_shared_memory;
     ma.is_load = is_load;
-    ma.SFR_id = *(int *)psyn_ops_counter;
+    ma.SFR_id = ((int *)psyn_ops_counter)[block_id];
+
+    /* collect memory address information from other threads */
+    int active_mask = ballot(1);
+    const int laneid = l_thread_id & 31;  /* use get_laneid() is less efficient */
+    const int first_laneid = __ffs(active_mask) - 1;
+
+    for (int i = 0; i < 32; i++) {
+        ma.addrs[i] = shfl(addr, i);
+    }
 
     /* first active lane pushes information on the channel */
     if (first_laneid == laneid) {
@@ -98,7 +72,6 @@ extern "C" __device__ __noinline__ void instrument_syn(uint64_t psyn_ops_counter
     int block_id = blockIdx.x + blockIdx.y * gridDim.x + gridDim.x * gridDim.y * blockIdx.z;
     if (threadIdx.x == 0 && threadIdx.y == 0 && threadIdx.z == 0) {
         atomicAdd(((int *)psyn_ops_counter) + block_id, 1);
-        printf("%d\n", ((int *)psyn_ops_counter)[block_id]);
     }
 
     // wait for other threads so can gurantee that the counter is updated after this function call
